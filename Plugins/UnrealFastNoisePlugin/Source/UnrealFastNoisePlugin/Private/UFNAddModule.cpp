37d384dbba1310d962bddc2c930c3d26c2de7421
Michael Chapman <woppin@gmail.com>
2016-09-13 02:25:02 +1000

    Clean up log line

--- Plugins/UnrealFastNoisePlugin/Source/UnrealFastNoisePlugin/Private/UFNAddModule.cpp
+++ Plugins/UnrealFastNoisePlugin/Source/UnrealFastNoisePlugin/Private/UFNAddModule.cpp
@@ -10,7 +10,6 @@ UUFNAddModule::UUFNAddModule(const FObjectInitializer& ObjectInitializer) : Supe
 
 float UUFNAddModule::GetNoise3D(float aX, float aY, float aZ)
 {
-
 	if (!(inputModule1 || inputModule2)) {
 		return 0.0f;
 	}
@@ -28,14 +27,30 @@ float UUFNAddModule::GetNoise3D(float aX, float aY, float aZ)
 		}
 	}
 
-
 	return modifier * (inputModule1->GetNoise3D(aX, aY, aZ) + inputModule2->GetNoise3D(aX, aY, aZ));
-
-
 }
+
 
 float UUFNAddModule::GetNoise2D(float aX, float aY)
 {
-	return GetNoise3D(aX, aY, 0.0f);
+	if (!(inputModule1 || inputModule2)) {
+		return 0.0f;
+	}
+
+	float modifier = 1.0f;
+	if (maskModule)
+	{
+		float mask = maskModule->GetNoise2D(aX, aY, aZ);
+		if (mask >= threshold)
+		{
+			modifier = mask;
+		}
+		else {
+			return inputModule1->GetNoise2D(aX, aY, aZ);
+		}
+	}
+
+
+	return modifier * (inputModule1->GetNoise2D(aX, aY, aZ) + inputModule2->GetNoise2D(aX, aY, aZ));
 }
 
